c3a2108f30c8eafbaad74a6e482a2aeef31a566e
shadreza <45334134+shadreza@users.noreply.github.com>
2021-04-19 02:41:37 +0600

    doing some to check heroku

--- index.js
+++ index.js
@@ -9,3 +9,15 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+const showTimes = () => {
+  let result = '';
+  const times = process.env.TIMES || 5;
+  let i =0;
+  for (i = 0; i < times; i++) {
+    result += i + ' ';
+  }
+  return result;
+}
+
+app.get('/times', (req, res) => res.send(showTimes()))
