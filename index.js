4e1a2c3d35e29318e4845b63c1cb6414772b8549
shadreza <45334134+shadreza@users.noreply.github.com>
2021-04-19 04:22:18 +0600

    Alhamdulillah Heroku Working by whitelisting network access in mongodb

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
-// const url = 'mongodb+srv://shikkhok-database-user-01:QJxXDF8OT9VtFXQQ@hay-store-cluster-01.coi91.mongodb.net/shikkhok-user-db?retryWrites=true&w=majority'
 const userDatabaseURI = `mongodb+srv://${process.env.databaseUser}:${process.env.databasePassword}@hay-store-cluster-01.coi91.mongodb.net/${process.env.userDatabase}?retryWrites=true&w=majority`
 const userClient = new MongoClient(userDatabaseURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -33,15 +32,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
-
-const showTimes = () => {
-  let result = '';
-  const times = process.env.TIMES || 5;
-  let i =0;
-  for (i = 0; i < times; i++) {
-    result += i + ' ';
-  }
-  return result;
-}
-
-app.get('/times', (req, res) => res.send(showTimes()))
