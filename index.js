8fff261da3efe28b0df1f0ff2543f23eede8ac0f
shadreza <45334134+shadreza@users.noreply.github.com>
2021-04-19 02:56:05 +0600

    testing heroku

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const port = process.env.PORT || 5055;
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+app.use(cors());
+app.use(bodyParser.json());
+
+const userDatabaseURI = `mongodb+srv://${process.env.databaseUser}:${process.env.databasePassword}@hay-store-cluster-01.coi91.mongodb.net/${process.env.userDatabase}?retryWrites=true&w=majority`
+const userClient = new MongoClient(userDatabaseURI, { useNewUrlParser: true, useUnifiedTopology: true });
+userClient.connect(err => {
+  const userCollection = userClient.db(`${process.env.userDatabase}`).collection("user");
+  app.get('/users', (req, res) => {
+    userCollection.find()
+      .toArray()
+      .then(items =>{
+        res.send(items);
+      })
+  })
+
+})
 
 app.get('/', (req, res) => {
   res.send('Shikkhok Server Site Locally Connected')
